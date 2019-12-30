96ae7a9bab40c89ee65b4f380f906bbb2be391e6
Hans Chua <h.g.n.chua@gmail.com>
2019-12-30 10:27:37 +0800

    Update Test

--- src/test/java/hc/parser/PartDataTest.java
+++ src/test/java/hc/parser/PartDataTest.java
@@ -1,29 +1,33 @@
+/**
+ * 
+ */
 package hc.parser;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
 
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+
+/**
+ */
 public class PartDataTest {
 
-  PartData data;
-
-  @Before
-  public void setup() {
-    data = new PartData();
-  }
-
   @Test
-  public final void testGetDataHTML() {
-    data.setDataHTML("something");
-    assertEquals("something", data.getDataHTML());
-  }
+  public void validateSettersAndGetters() {
+    PojoClass data = PojoClassFactory.getPojoClass(PartData.class);
 
-  @Test
-  public final void testSetDataHTML() {
-    data.setDataHTML("something");
-    assertEquals("something", data.getDataHTML());
+    Validator validator = ValidatorBuilder.create()
+                                          .with(new SetterMustExistRule(), new GetterMustExistRule())
+                                          .with(new SetterTester(), new GetterTester())
+                                          .build();
+
+    validator.validate(data);
   }
 
 }
